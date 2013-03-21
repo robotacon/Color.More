a515aa0ec758abf169e38fa2fce1302dbca971de
robotacon <tor.viktorsson@gmail.com>
2013-03-21 10:33:56 +0100

    Added overlay, toHex and $NORMAL

--- Source/Color.More.js
+++ Source/Color.More.js
@@ -18,13 +18,17 @@ requires:
   - Core/Hash
   - Core/Function
   - MooTools.More
+  - Color
 
 provides:
 - Color.multiply
 - Color.screen
+- Color.overlay
 - Color.dim
 - Color.light
 - Color.shine
+- Color.toHex
+- $NORMAL
 
 ...
 */
@@ -64,6 +68,30 @@ Color.implement({
                 var top = rgb[i];
                 var bottom = color[i];
                 rgb[i] = Math.round(255 - (((255 - top) * (255 - bottom)) / 255));
+            }
+        });
+        return new Color(rgb, 'rgb');
+    },
+
+    /**
+     * Creates a color by overlaying two or more colors.
+     * Returns as new color.
+     * @param A single or many colors, in hex or rgb representation, to overlay on this Color
+     * @return Color
+     */
+    overlay: function () {
+        var colors = Array.slice(arguments);
+        var rgb = this.slice();
+        colors.each(function (color) {
+            color = new Color(color);
+            for (var i = 0; i < 3; i++) {
+                var top = rgb[i];
+                var bottom = color[i];
+                if (top / 255 < 0.5) {
+                    rgb[i] = 2 * Math.round(top * bottom / 255);
+                } else {
+                    rgb[i] = Math.round(255 - 2 * (((255 - top) * (255 - bottom)) / 255));
+                }
             }
         });
         return new Color(rgb, 'rgb');
@@ -120,5 +148,31 @@ Color.implement({
             }
         }
         return new Color(rgb, 'rgb');
+    },
+
+    toHex: function () {
+        var rgb = this.slice();
+        return "#" + (rgb[0]<10?"0":"") + rgb[0].toString(16) + (rgb[1]<10?"0":"") + rgb[1].toString(16) + (rgb[2]<10?"0":"") + rgb[2].toString(16);
     }
+
 });
+
+/*
+* Creates a color based on the normal vector at a point on a spherical surface
+* Returns as new color.
+* @param the x coordinate corresponding to a 2D projection of a sphere
+* @param the y coordinate corresponding to a 2D projection of a sphere
+* @param the size of the projection is the diameter of the sphere 
+* @return Color
+*/
+this.$NORMAL = function (x, y, size) {
+    var w = x / (size / 2) - 1 + 1 / size;
+    var h = y / (size / 2) - 1 + 1 / size;
+    var d = Math.sqrt(1 - w * w - h * h);
+
+    if ((1 >= w && w >= -1) && (1 >= h && h >= -1) && (1 >= d && d >= -1)) {
+        return new Color([parseInt(w * 128 + 127), parseInt(h * 128 + 127), parseInt(d * 128 + 127)], 'rgb');
+    } else {
+        return new Color([127, 127, 255], 'rgb');
+    }
+};
